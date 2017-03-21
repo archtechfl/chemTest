f271f1e67b58bfdc965435b1d855e78a3114b2a0
Jeremy Moore <archtechfl@gmail.com>
2017-03-21 10:30:47 -0400

    added all descriptions

--- data/descriptions.js
+++ data/descriptions.js
@@ -5,5 +5,24 @@ export const descriptions = {
 		2: "Must be moderately heated or exposed to relatively high ambient temperature before ignition can occur and multiple finely divided suspended solids that do not require heating before ignition can occur. Flash point between 37.8 and 93.3 °C (100 and 200 °F).",
 		3: "Liquids and solids (including finely divided suspended solids) that can be ignited under almost all ambient temperature conditions. Liquids having a flash point below 22.8 °C (73 °F) and having a boiling point at or above 37.8 °C (100 °F) or having a flash point between 22.8 and 37.8 °C (73 and 100 °F).",
 		4: "Will rapidly or completely vaporize at normal atmospheric pressure and temperature, or is readily dispersed in air and will burn readily (e.g. acetylene, propane, liquid hydrogen). Includes pyrophoric substances. Flash point below room temperature at 22.8 °C (73 °F)."
+	},
+	"reactivity": {
+		0: "Normally stable, even under fire exposure conditions, and is not reactive with water.",
+		1: "Normally stable, but can become unstable at elevated temperatures and pressures.",
+		2: "Undergoes violent chemical change at elevated temperatures and pressures, reacts violently with water, or may form explosive mixtures with water.",
+		3: "Capable of detonation or explosive decomposition but requires a strong initiating source, must be heated under confinement before initiation, reacts explosively with water, or will detonate if severely shocked.",
+		4: "Readily capable of detonation or explosive decomposition at normal temperatures and pressures."
+	},
+	"health": {
+		0: "Poses no health hazard, no precautions necessary and would offer no hazard beyond that of ordinary combustible materials.",
+		1: "Exposure would cause irritation with only minor residual injury.",
+		2: "Intense or continued but not chronic exposure could cause temporary incapacitation or possible residual injury.",
+		3: "Short exposure could cause serious temporary or moderate residual injury.",
+		4: "Very short exposure could cause death or major residual injury."
+	},
+	"special": {
+		w: "Reacts with water in an unusual or dangerous manner.",
+		ox: "Oxidizer, allows chemicals to burn without an air supply.",
+		sa: "Simple asphyxiant gas."
 	}
 }
